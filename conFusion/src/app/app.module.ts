002eb97e11ee4f5772272618aed953ff81b38be9
Jitu <Jitu@Jitus-MacBook-Air.local>
2019-07-22 09:18:25 +0530

    Assignment 2

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -21,6 +21,7 @@ import { DishService } from './services/dish.service';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { PromotionService } from './services/promotion.service';
+import { LeaderService } from './services/leader.service';
 
 @NgModule({
   declarations: [
@@ -44,7 +45,7 @@ import { PromotionService } from './services/promotion.service';
     MatButtonModule,
     AppRoutingModule
   ],
-  providers: [ DishService, PromotionService ],
+  providers: [ DishService, PromotionService, LeaderService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
